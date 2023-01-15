20c774da2f61f4c854fe77070c3448a7b89a95d7
EzequielAbade <krauser_ezequiel@hotmail.com>
2023-01-15 11:04:53 -0400

    Aulas JavaScript

---
+++ JavaScript/FUNDAMENTOS/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)
+console.log('10' / 2) //converteu mesmo sendo string
+console.log("show" * 2) // vai gerar um erro
+console.log(0.1 + 0.7)
+// console.log(10.toString()) gera erro
+console.log((10.345).toFixed(2)) //  Mas neste caso ele gera normalmente
